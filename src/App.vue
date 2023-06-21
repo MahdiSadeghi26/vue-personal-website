d5093c8c12a4be3ea08d91d289e40bb4560ca3c9
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-06-21 13:50:22 +0330

    background applied!

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,10 @@ export default {
 </script>
 
 <style lang="scss">
+body{
+  background: url("../public/image/background.jpg")no-repeat fixed center;
+  background-size: cover;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,5 +22,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  
 }
 </style>

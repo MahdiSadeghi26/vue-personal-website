6dc8236af0002e52e8cbcba3c78c7e0b22d3bfb0
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-06-25 11:19:45 +0330

    face cmp designed init

---
+++ src/components/Face.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="faceContainer">
+        <div class="eclipse">
+            <img src="../../public/image/eclipsebkg.png" alt="">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss">
+.faceContainer {
+    grid-column: 2;
+    grid-row: 1 /3;
+    justify-self: end;
+    align-content: end;
+    height: 100%;
+    .eclipse{
+        width: 400px;
+        grid-column: 2;
+        opacity: .6;
+        img{
+            width: 100%;
+        }
+    }
+}
+</style>

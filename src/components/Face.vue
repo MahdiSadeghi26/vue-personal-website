0f5332d8219d919cc25ed5304c8326940a0c98d7
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-06-25 19:28:30 +0330

    social media section added (have placement issue)

--- src/components/Face.vue
+++ src/components/Face.vue
@@ -8,6 +8,15 @@
         <div class="face">
             <img class="faceImg" src="../../public/image/face.png" alt="Image not loaded">
         </div>
+        <!-- social Media seciton -->
+        <div class="socialMedia">
+            <!-- instargram logo -->
+            <a target="_blank" class="igLogo  icon" href="https://www.instagram.com/mahdi_sadeghi18/"><img src="../../public/image/ig-logo.png" alt="instagram Logo"></a>
+            <!-- twitter logo -->
+            <a target="_blank" class="twitterLogo icon" href="https://twitter.com/Mehdi_Fyber"><img src="../../public/image/twitter-logo.png" alt="Twitter logo"></a>
+            <!-- Github Logo -->
+            <a target="_blank" class="githubLogo icon" href="https://github.com/MahdiSadeghi26"><img src="../../public/image/github-logo.png" alt=""></a>
+        </div>
     </div>
 </template>
 <script>
@@ -18,28 +27,42 @@ export default {
 <style lang="scss">
 .faceContainer {
     bottom: 10px;
+
     // purple seciton (background)
-    .eclipse{
-        right: 0;
+    .eclipse {
         position: absolute;
+        right: 0;
         width: 400px;
-        opacity: .8;
+        opacity: .7;
         bottom: 0;
-        z-index: -1;
-        img{
+        img {
             width: 100%;
         }
     }
     // face seciton
-    .face{
+    .face {
         position: absolute;
         right: 0;
         bottom: 0;
-        img{
+
+        img {
             width: 380px;
             height: 550px;
         }
     }
-    
+    .socialMedia{
+        position: absolute;
+        right: 10px;
+        top:12rem;
+        
+        a{
+            display: block;
+            margin-bottom: 15px;
+            img{
+                width: 40px;
+            }
+        }
+    }
+
 }
 </style>

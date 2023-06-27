5f0cf2251f2c40ce85723ea181e046f28f09382e
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2023-06-27 13:58:41 +0330

    Moved the fonts to cdn

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
-<script setup>
- 
-</script>
+<script setup></script>
 
 <template>
   <div class="container">
-   <h1>salam</h1>
+    <h1 class="title">salam</h1>
   </div>
 </template>
 
 <style scoped lang="scss">
-.container{
-    background: url("../../../public/image/background.jpg")no-repeat fixed center;
-    background-size: cover;
-    width: 100vw;
-    height: 100vh;
-    h1{
+.container {
+  background: url("/image/background.jpg") no-repeat fixed center;
+  background-size: cover;
+  width: 100vw;
+  height: 100vh;
+  .title {
     color: white;
     font-family: "Montserrat";
     font-weight: 100;

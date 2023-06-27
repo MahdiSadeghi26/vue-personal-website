280b8aa87479afead608d5c555e99b2e4f84c29b
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-06-27 13:26:44 +0330

    fonts path changed to ./ app.vue style added to self cmp

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,19 @@
 
 <template>
   <div class="container">
-   
+   <h1>salam</h1>
   </div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.container{
+    background: url("../../../public/image/background.jpg")no-repeat fixed center;
+    background-size: cover;
+    width: 100vw;
+    height: 100vh;
+  h1{
+    color: white;
+    font-family: Montserrat-Thin;
+  }
+}
 </style>

9f965953aae0b3901f2658d4618f048ccd9e45d6
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-06-27 17:41:47 +0330

    layoting main container and styling title

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,38 @@
-<script setup></script>
+<script setup>
+ 
+</script>
 
 <template>
   <div class="container">
-    <h1 class="title">salam</h1>
+   <div class="leftSide"><h1>Mehdi Sadeghi</h1></div>
+   <div class="rightSide"></div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.container {
-  background: url("/image/background.jpg") no-repeat fixed center;
-  background-size: cover;
-  width: 100vw;
-  height: 100vh;
-  .title {
-    color: white;
-    font-family: "Montserrat";
-    font-weight: 100;
-  }
+.container{
+    background: url("/image/background.jpg")no-repeat fixed center;
+    background-size: cover;
+    width: 100vw;
+    height: 100vh;
+    display: grid;
+    grid-template-columns: 50% 50%;
+    .leftSide{
+      display: flex;
+      flex-direction: column;
+      padding: 30px 0 15px 41px;
+      h1{
+      color: #8493BF;
+      font-family: "Montserrat";
+      font-weight: 700;
+      font-size: 30px;
+      &:hover{
+        color: #9caee4;
+      }
+    }
+    }
+    .rightSide{
+      
+    }
 }
 </style>

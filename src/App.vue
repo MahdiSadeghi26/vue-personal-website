b89423f8cae82e4b7d52d406c878a9a2d5ec3dbb
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-06-27 18:27:36 +0330

    page dvided 2 div skills cmp added

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script setup>
- 
+ import Skills from './components/Skills.vue';
 </script>
 
 <template>
+  <!-- main container -->
   <div class="container">
-   <div class="leftSide"><h1>Mehdi Sadeghi</h1></div>
+    <!-- left side container -->
+    
+   <div class="leftSide">
+    <!-- header name -->
+    <h1>Mehdi Sadeghi</h1>
+    <!-- middle page fullName -->
+    <div class="fullName">
+      <p class="fName">Mehdi</p>
+      <p class="lName">Sadeghi</p>
+    </div>
+  <Skills/>
+  
+  </div>
+   <!-- Right side Container -->
    <div class="rightSide"></div>
   </div>
 </template>
@@ -20,16 +34,21 @@
     .leftSide{
       display: flex;
       flex-direction: column;
+      justify-content: space-between;
       padding: 30px 0 15px 41px;
       h1{
       color: #8493BF;
-      font-family: "Montserrat";
       font-weight: 700;
       font-size: 30px;
       &:hover{
         color: #9caee4;
       }
-    }
+      }
+      .fullName{
+        color: #CEB3F2;
+        font-weight: 700;
+        font-size: 70px;
+      }    
     }
     .rightSide{
       

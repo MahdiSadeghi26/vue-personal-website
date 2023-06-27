bae7a75af09b56f040a86bd902b433f1943b5d0c
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-06-27 18:29:46 +0330

    Skills cmp designed (part 1)

---
+++ src/components/Skills.vue
@@ -0,0 +1,52 @@
+<template >
+    <div class="skillsContainer">
+        <!-- job title -->
+        <p class="jobTitle">Junior Front-End Developer</p>
+        <!-- skills i have -->
+        <p class="mySkills">HTML  |  CSS  |  JS  |  Vue</p>
+        <!-- hire me button -->
+        <button class="hireBtn"><a href="mailto:mahdisadeghi1381m@gmail.com">Hire Me !</a></button>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+    .skillsContainer{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        color: #ffff;
+        margin-bottom: 40px;
+        width: 420px;
+        .jobTitle{
+            font-weight: 400;
+            font-size: 30px;
+            margin-bottom: 20px;
+        }
+        .mySkills{
+            font-weight: 100;
+            font-size: 26px;
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        .hireBtn{
+            background-color: #8A7ED9;
+            border: none;
+            padding: 15px 50px;
+            border-radius: 25px;
+            font-size: 20px;
+            align-self: center;
+            font-weight: 600;
+            &:hover{
+                background-color: #a79de7;
+            }
+            a{
+                text-decoration: none;
+                color: #ffff;
+            }
+        }
+    }
+</style>

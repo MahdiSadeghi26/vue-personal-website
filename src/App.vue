08c5d6c316e00e369c5a24345eb6eb1bb4380d32
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-06-28 12:03:03 +0330

    social Media section added

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,117 @@
 <script setup>
- import Skills from './components/Skills.vue';
+import Skills from './components/Skills.vue';
 </script>
 
 <template>
   <!-- main container -->
   <div class="container">
     <!-- left side container -->
-    
-   <div class="leftSide">
-    <!-- header name -->
-    <h1>Mehdi Sadeghi</h1>
-    <!-- middle page fullName -->
-    <div class="fullName">
-      <p class="fName">Mehdi</p>
-      <p class="lName">Sadeghi</p>
+
+    <div class="leftSide">
+      <!-- header name -->
+      <h1>Mehdi Sadeghi</h1>
+      <!-- middle page fullName -->
+      <div class="fullName">
+        <p class="fName">Mehdi</p>
+        <p class="lName">Sadeghi</p>
+      </div>
+      <Skills />
+
     </div>
-  <Skills/>
-  
-  </div>
-   <!-- Right side Container -->
-   <div class="rightSide">
-    <!-- purple ecplipse -->
-    <img class="eclipse" src="/public/image/eclipse.png" alt="image 404">
-    <!-- face -->
-    <img src="/public/image/face.png" alt="Image 404" class="face">
-   </div>
+    <!-- Right side Container -->
+    <div class="rightSide">
+      <!-- purple ecplipse -->
+      <img class="eclipse" src="/public/image/eclipse.png" alt="image 404">
+      <!-- face -->
+      <img src="/public/image/face.png" alt="Image 404" class="face">
+      <!-- social media section -->
+      <div class="socialMedia">
+        <!-- instagram -->
+        <a target="_blank" href="https://www.instagram.com/mahdi_sadeghi18/" class="instagram" title="My Instagram">
+          <img class="instagramLogo" src="/public/image/ig-logo.png" alt="Image 404"></a>
+        <!-- twitter -->
+        <a target="_blank" href="https://twitter.com/Mehdi_Fyber" title="My Twitter" class="twitter">
+          <img class="twitterLogo" src="/public/image/twitter-logo.png" alt="Image 404"></a>
+        <!-- gitgub -->
+        <a target="_blank" href="https://github.com/MahdiSadeghi26" title="My Github" class="github">
+          <img class="githubLogo" src="/public/image/github-logo.png" alt="Image 404"></a>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.container{
-    background: url("/image/background.jpg")no-repeat fixed center;
-    background-size: cover;
-    width: 100vw;
-    height: 100vh;
-    display: grid;
-    grid-template-columns: 50% 50%;
-    // left side section
-    .leftSide{
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      padding: 30px 0 15px 41px;
-      h1{
+.container {
+  background: url("/image/background.jpg")no-repeat fixed center;
+  background-size: cover;
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 50% 50%;
+
+  // left side section
+  .leftSide {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 30px 0 15px 41px;
+
+    h1 {
       color: #8493BF;
       font-weight: 700;
       font-size: 30px;
-      &:hover{
+
+      &:hover {
         color: #9caee4;
       }
-      }
-      .fullName{
-        color: #CEB3F2;
-        font-weight: 700;
-        font-size: 70px;
-      }    
     }
-    // Right side section
-    .rightSide{
-      position: relative;
-      .eclipse{
-        position: absolute;
-        bottom: 0;
-        right: 0;
-        width: 40%;
-        height:50%;
-        min-height: 80vh;
-        opacity: .7;
-      }
-      .face{
-        position: absolute;
-        right: 10px;
-        bottom: 0;
-        width: 35%;
-        aspect-ratio: auto;
+
+    .fullName {
+      color: #CEB3F2;
+      font-weight: 700;
+      font-size: 70px;
+    }
+  }
+
+  // Right side section
+  .rightSide {
+    position: relative;
+
+    .eclipse {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      width: 40%;
+      height: 50%;
+      min-height: 80vh;
+      opacity: .7;
+    }
+
+    .face {
+      position: absolute;
+      right: 10px;
+      bottom: 0;
+      width: 35%;
+      aspect-ratio: auto;
+    }
+    // social Media
+    .socialMedia{
+      position: absolute;
+      right: 10px;
+      top: 25%;
+      a{
+        display: block; 
+        img{
+          width: 35px;
+          height: auto;
+          margin: 10px 0;
+        }
+        .githubLogo{
+          width: 40px;
+        }
       }
     }
+  }
 }
 </style>
